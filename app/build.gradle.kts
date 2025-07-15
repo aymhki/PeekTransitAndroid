1391e7ec3cc6eb32883fd57fe0907fb3e4f114d8
aymanhki <agamyahk@myumanitoba.ca>
2025-07-15 01:10:39 -0500

    Some bug fixes

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.aymanhki.peektransit"
         minSdk = 31
         targetSdk = 36
-        versionCode = 5
-        versionName = "1.1"
+        versionCode = 7
+        versionName = "1.2"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
